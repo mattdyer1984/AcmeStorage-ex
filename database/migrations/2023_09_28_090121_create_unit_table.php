3cb0d6194d4dfdb7e92b3c6ff447566cc5785c6e
mattdyer1984 <matt@bristol-remaps.co.uk>
2023-09-28 10:24:29 +0100

    units table created

--- database/migrations/2023_09_28_090121_create_unit_table.php
+++ database/migrations/2023_09_28_090121_create_unit_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('description', 500)->nullable();
+            $table->integer('volume')->unsigned();
+            $table->boolean('available')->default(true);
             $table->timestamps();
         });
     }
